b2c188d3d208ad7913b8e9f1feb1685930865fe8
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2020-02-20 10:49:25 +0000

    Tidy up for adding solution

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,8 @@ static void canWriteTask(void);
 static void canReadTask(void);
 static void canHandler(void);
 
-int main () {
+int main()
+{
     osStatus status;
 
     red = 0;
@@ -42,53 +43,58 @@ int main () {
     assert(osOK == status);
 
     while (true) {
-      led1Toggle();
-      wait_ms(500);
+        led1Toggle();
+        wait_ms(500);
     }
 }
 
-static void led1Toggle(void) {
-  red = 1 - red;
+static void led1Toggle(void)
+{
+    red = 1 - red;
 }
 
 /* Transmit CAN message with arbitrary id and 8 bytes of
  * data consisting of a repeated count of the number of transmitted messages
- */ 
-void canWriteTask(void) {
-  
-  static canMessage_t txMsg = {0x23, 8, 0, 0}; 
-  bool txOk;
-    
-  while (true) {
-      // Transmit message on CAN 
-      txOk = canWrite(&txMsg);
-      if (txOk) {
-	txCount += 1;
-	txMsg.dataA = txCount;
-	txMsg.dataB = txCount;
-      }
-      wait_ms(250);
-  }
+ */
+void canWriteTask(void)
+{
+
+    static canMessage_t txMsg = { 0x23, 8, 0, 0 };
+    bool txOk;
+
+    while (true) {
+        // Transmit message on CAN 
+        txOk = canWrite(&txMsg);
+        if (txOk) {
+            txCount += 1;
+            txMsg.dataA = txCount;
+            txMsg.dataB = txCount;
+        }
+        wait_ms(250);
+    }
 }
 
 /* Read and display messages arriving on the CAN port */
-void canReadTask(void) {
-  canRxInterrupt(canHandler); // configure CAN to interrupt on message reception
+void canReadTask(void)
+{
+    canRxInterrupt(canHandler); // configure CAN to interrupt on message reception
 
-  rxDone = false;
-  while (true) {
-      if (rxDone) { // rxDone could be better handled by a semaphore
-	rxDone = false;
-        pc.printf("ID: 0x%lx LEN: 0x%01lx DATA_A: 0x%08lx DATA_B: 0x%08lx\n", rxMsg.id, rxMsg.len, rxMsg.dataA, rxMsg.dataB); 
-        rxCount += 1;
-      }
-      wait_ms(100);
-  }
+    rxDone = false;
+    while (true) {
+        if (rxDone) {           // rxDone could be better handled by a semaphore
+            rxDone = false;
+            pc.printf
+                ("ID: 0x%lx LEN: 0x%01lx DATA_A: 0x%08lx DATA_B: 0x%08lx\n",
+                 rxMsg.id, rxMsg.len, rxMsg.dataA, rxMsg.dataB);
+            rxCount += 1;
+        }
+        wait_ms(100);
+    }
 }
 
 /* A simple interrupt handler for CAN message reception */
-void canHandler(void) {
+void canHandler(void)
+{
     canTransferRxFrame(&rxMsg);
     rxDone = true;
 }
-    
