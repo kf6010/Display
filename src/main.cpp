3100c2d9efbeed755c7a5ae075b6015901548427
Dr Alun Moon <alun@orac.moonbase>
2020-03-27 16:22:17 +0000

    Added template for assignment. TODO comments indicate code the needs writing. Additional guidance is needed for those social-distancing under the current covid-19 guidelines.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,17 @@
  * DK - 02-Feb-2019
  */
 
+/* TODO:
+ * comments like this give some additional guidance for the assignment
+ * with a view to working alone rather than in a pair.
+ * Reflecting the current distancing situation.
+ *                                              Alun     27 March 2020
+ *   I've commented out sections of code that are
+ *   unneeded for the assignment.
+ *   Please delete commented-out, unneeded, and redundant sections
+ *   of code before submitting the source.
+ *   It makes the job of marking and reviewing easier.
+ */
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -21,26 +32,64 @@ static uint32_t rxCount;
 static volatile canMessage_t rxMsg;
 static volatile bool rxDone;
 
+/* unneeded threads
 Thread writer(osPriorityRealtime);
 Thread reader(osPriorityRealtime1);
+*/
+
+/* TODO:
+ * Declare threads for the RequestT and DisplayT tasks
+ */
+
 
 static void led1Toggle(void);
+
+/* TODO: you still need the CAN interrupt handler */
+static void canHandler(void);
+
+/* unneeded headers 
 static void canWriteTask(void);
 static void canReadTask(void);
-static void canHandler(void);
+*/
+
+/* TODO:
+ * You need two tasks(threads)
+ *     RequestT
+ *     DisplayT
+ */
+void RequestT(void);
+void DisplayT(void);
 
 int main()
 {
     osStatus status;
 
     red = 0;
-    canInit(BD125000, true);
+
+	/* TODO:
+	 * Normally you would CAN connect two boards,
+	 * If you are working alone, or with a single board
+	 * then you will have to use the CAN adaptor in
+	 * `loopback' mode.
+	 */
+    canInit(BD125000, false);
+
     pc.printf("Display -- Loopback test\n");
 
-    status = reader.start(canReadTask);
-    assert(osOK == status);
-    status = writer.start(canWriteTask);
-    assert(osOK == status);
+	/* YOU don't need the read and write tasks from the exercises.
+	 * but they ARE good examples to follow
+	 */
+	/* comment out unneeded code:
+        status = reader.start(canReadTask);
+        assert(osOK == status);
+        status = writer.start(canWriteTask);
+        assert(osOK == status);
+	*/
+
+	/* TODO:
+	 * Create and start RequestT and DisplayT tasks
+	 */
+
 
     while (true) {
         led1Toggle();
@@ -48,6 +97,63 @@ int main()
     }
 }
 
+
+/* TODO:
+ * A simple interrupt handler for CAN message reception.
+ * Ideally this should release DisplayT using a semaphore
+ * when a message is available
+ */
+void canHandler(void)
+{
+    canTransferRxFrame(&rxMsg);
+    rxDone = true;
+}
+
+/* TODO:
+ * Periodic task to send ReqT messages to TempT task.
+ */
+void RequestT(void)
+{
+	/* TODO:
+	 * populate message with ID and fields
+	 */
+	static canMessage_t ReqT;
+
+	while(true) { /* OS Thread so needs while-true loop */
+		/* TODO:
+		 * Send the ReqT message.
+		 * If you are using the loopback device, the message is echoed back as 
+		 * the AckT message so populate it with some data as if from TempT in
+		 * EngineMonitor.
+		 */
+
+		/* TODO:
+		 * Task should have a 500ms period,
+		 * use the appropriate delay here.
+		 */
+	}
+}
+
+/* TODO:
+ * Task to respond to AckT messages,
+ * unpack the data from the message and display information
+ * on the pc serial communications.
+ */
+void DisplayT(void)
+{
+	/* TODO:
+	 * declare needed data and variables
+	 */
+	static canMessage_t AckT;
+
+	while(true) { /* OS Thread so needs while-true loop */
+		/* TODO: wait for semaphore */
+		/* TODO: unpack data from AckT message */
+		/* TODO: display values on PC */
+	}
+}
+
+/*-------------------------------*/
 static void led1Toggle(void)
 {
     red = 1 - red;
@@ -56,6 +162,7 @@ static void led1Toggle(void)
 /* Transmit CAN message with arbitrary id and 8 bytes of
  * data consisting of a repeated count of the number of transmitted messages
  */
+/*
 void canWriteTask(void)
 {
 
@@ -73,8 +180,10 @@ void canWriteTask(void)
         wait_ms(250);
     }
 }
+*/
 
 /* Read and display messages arriving on the CAN port */
+/*
 void canReadTask(void)
 {
     canRxInterrupt(canHandler); // configure CAN to interrupt on message reception
@@ -91,10 +200,4 @@ void canReadTask(void)
         wait_ms(100);
     }
 }
-
-/* A simple interrupt handler for CAN message reception */
-void canHandler(void)
-{
-    canTransferRxFrame(&rxMsg);
-    rxDone = true;
-}
+*/
